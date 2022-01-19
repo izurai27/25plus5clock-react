d6f0c83398ab45446ebd871525a1194200e15613
Izma Raifani <izurai@gmail.com>
2022-01-19 13:33:57 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      session:25,
+      break:5,
+      seconds:0,
+      counting:false
+    }
+    
+    this.handleBreakDecrement=this.handleBreakDecrement.bind(this);
+    this.handleBreakIncrement=this.handleBreakIncrement.bind(this);
+    this.handleSessionDecrement=this.handleSessionDecrement.bind(this);
+    this.handleSessionIncrement=this.handleSessionIncrement.bind(this);
+    this.handleReset=this.handleReset.bind(this);
+    this.handleStart=this.handleStart.bind(this);
+  }
+
+  handleBreakIncrement(event){
+    if(this.state.break<60){
+      this.setState({
+        break:this.state.break+1
+      })
+    }
+  }
+
+  handleBreakDecrement(event){
+    if(this.state.break>1){
+      this.setState({
+      break:this.state.break-1
+    })
+    }
+    
+  }
+  
+  handleSessionIncrement(event){
+    if(this.state.session<60){
+      this.setState({
+        session:this.state.session+1
+      })
+    }
+  }
+  
+  handleSessionDecrement(event){
+    if(this.state.session>1){
+      this.setState({
+        session:this.state.session-1
+      })
+    }
+  }
+
+  handleReset(event){
+    this.setState({
+      session:25,
+      break:5
+    })
+  }
+
+  handleStart(event){
+    console.log("it's start");
+    this.setState({counting : !this.state.counting})
+    if (this.state.counting === true){
+      setInterval( () => this.setState ({session:this.state.session-1 }), 1000);
+    }
+    console.log(this.state.counting)
+       
+  }
+
+  render(){
+    return(
+      <div> 
+        <div id="timer-label">SESSION</div>
+        <time id="time-left">{this.state.session}</time>
+        <button id="start_stop"onClick={this.handleStart}>start</button>
+        <button id="reset" onClick={this.handleReset}>reset</button>
+
+        <div>Change Session or Break Length:</div>
+        <div id="break-label">Break Length (minutes)</div>
+        <div id="break-length">{this.state.break}</div>
+        <button id="break-increment" onClick={this.handleBreakIncrement}>more</button>
+        <button id="break-decrement" onClick={this.handleBreakDecrement}>less</button>
+
+        <div id="session-label">SESSION LENGTH</div>
+        <div id="session-length">{this.state.session}</div>
+        <button id="session-increment" onClick={this.handleSessionIncrement}>more</button>
+        <button id="session-decrement" onClick={this.handleSessionDecrement}>less</button>
+        
+      </div>
+     
+    )
+  }
 }
 
 export default App;
