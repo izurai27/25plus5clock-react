01af20d5a5234724acbcf11da6d5c65b333f8cc7
Izma Raifani <izurai@gmail.com>
2022-01-20 11:42:48 +0700

    add styling

--- src/App.js
+++ src/App.js
@@ -73,23 +73,35 @@ class App extends React.Component {
 
   render(){
     return(
-      <div> 
-        <div id="timer-label">SESSION</div>
-        <time id="time-left">{this.state.session}</time>
-        <button id="start_stop"onClick={this.handleStart}>start</button>
-        <button id="reset" onClick={this.handleReset}>reset</button>
-
-        <div>Change Session or Break Length:</div>
-        <div id="break-label">Break Length (minutes)</div>
-        <div id="break-length">{this.state.break}</div>
-        <button id="break-increment" onClick={this.handleBreakIncrement}>more</button>
-        <button id="break-decrement" onClick={this.handleBreakDecrement}>less</button>
-
-        <div id="session-label">SESSION LENGTH</div>
-        <div id="session-length">{this.state.session}</div>
-        <button id="session-increment" onClick={this.handleSessionIncrement}>more</button>
-        <button id="session-decrement" onClick={this.handleSessionDecrement}>less</button>
+      <div className="big-container">
+        <div className="count-down">
+          <div id="timer-label">SESSION</div>
+          <time id="time-left">{this.state.session}</time>
+          <div className='buttons'>
+            <button id="start_stop"onClick={this.handleStart}>start</button>
+          <button id="reset" onClick={this.handleReset}>reset</button>
+          </div>
+          
+        </div> 
         
+        <div className="edit-value">
+          <div className="edit-label">Change Session or Break Length:</div>
+          
+          <div className='edit-session'>
+            <div id="session-label">SESSION LENGTH</div>
+            <div id="session-length">{this.state.session}</div>
+            <button id="session-increment" onClick={this.handleSessionIncrement}>more</button>
+            <button id="session-decrement" onClick={this.handleSessionDecrement}>less</button>
+                    
+          </div>
+
+          <div className='edit-break'>
+            <div id="break-label">Break Length (minutes)</div>
+            <div id="break-length">{this.state.break}</div>
+            <button id="break-increment" onClick={this.handleBreakIncrement}>more</button>
+            <button id="break-decrement" onClick={this.handleBreakDecrement}>less</button>
+          </div>
+        </div>
       </div>
      
     )
